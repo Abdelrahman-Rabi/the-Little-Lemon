ab69258d6f8b9d0b2673ca6c5a432f77f04e8cf2
Faris Rabie <farisrabie@Fariss-MacBook-Pro.local>
2024-07-30 17:18:56 +0300

    create NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { Route, Routes } from "react-router-dom";
 // import Layout from "./components/layout/Layout";
 import Home from "./components/pages/Home/Home";
 import Reservations from "./components/pages/Reservations/Reservations";
-// import ConfirmedReservation from "./components/pages/Reservations/confirmedReservation";
-// import NotFound from "./components/pages/NotFound/NotFound";
+import ConfirmedReservation from "./components/pages/Reservations/confirmedReservation";
+import NotFound from "./components/pages/NotFound/NotFound";
 
 const App = () => {
   return (
@@ -12,11 +12,11 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/reservations" element={<Reservations />} />
-          {/* <Route
+          <Route
             path="/confirmedReservation"
             element={<ConfirmedReservation />}
           />
-          <Route path="*" element={<NotFound />} /> */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
     </>
   );
