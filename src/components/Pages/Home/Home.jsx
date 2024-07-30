dbef9827ad6e58762585b804969fb8be66679f5e
Faris Rabie <farisrabie@Fariss-MacBook-Pro.local>
2024-07-30 14:52:14 +0300

    create the Home page

---
+++ src/components/Pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import Hero from "./Hero";
+import WeekSpecials from "./WeekSpecials";
+import Testimonials from "./TestimonialsCard";
+import OurStory from "./OurStory";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <WeekSpecials />
+      <Testimonials />
+      <OurStory />
+    </>
+  );
+};
+
+export default Home;
