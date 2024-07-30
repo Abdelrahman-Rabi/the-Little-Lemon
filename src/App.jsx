4ecb065ac2135ba3516ee6b42d016ed2234660d8
Faris Rabie <farisrabie@Fariss-MacBook-Pro.local>
2024-07-30 17:16:09 +0300

    create Reservations

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 // import Layout from "./components/layout/Layout";
 import Home from "./components/pages/Home/Home";
-// import Reservations from "./components/pages/Reservations/Reservations";
+import Reservations from "./components/pages/Reservations/Reservations";
 // import ConfirmedReservation from "./components/pages/Reservations/confirmedReservation";
 // import NotFound from "./components/pages/NotFound/NotFound";
 
@@ -10,9 +10,9 @@ const App = () => {
     <>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/home" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/reservations" element={<Reservations />} />
-          <Route
+          {/* <Route
             path="/confirmedReservation"
             element={<ConfirmedReservation />}
           />
